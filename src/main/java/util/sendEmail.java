540946030b96254c3161cffbe3b28ba76dd7604a
leosmsilvx <leosmsilvx@gmail.com>
2022-11-03 12:46:47 -0300

    Small changes html email

--- src/main/java/util/sendEmail.java
+++ src/main/java/util/sendEmail.java
@@ -17,21 +17,23 @@ public class sendEmail {
 				+ "    color: black;\r\n"
 				+ "}\r\n"
 				+ ".container{\r\n"
-				+ "    margin-left: 25%;\r\n"
-				+ "	max-width: 40%;\r\n"
+				+ "    margin-left: 10%;\r\n"
+				+ "	max-width:60%;\r\n"
 				+ "}\r\n"
 				+ "</style>\r\n"
 				+ "        </head>\r\n"
 				+ "        <body>\r\n"
 				+ "			<div class=\"container\">\r\n"
-				+ "				<div style=\"padding-top: 5%; padding-bottom: 3%;\">\r\n"
-				+ "                    <a style=\"text-align: center; font-size: 1.5em; margin-top: 0.83em; margin-bottom: 0.53em; font-weight: bold; display: block;\">iTonomise</a><br><br>\r\n"
+				+ "				<div style=\"padding-top: 2%; padding-bottom: 1%;\">\r\n"
+				+ "                    <a style=\"text-align: center; font-size: 1.5em; margin-top: 0.83em; margin-bottom: 0.2em; font-weight: bold; display: block;\">iTonomise</a><br><br>\r\n"
 				+ "                </div>\r\n"
 				+ "                <div>\r\n"
 				+ "                    <a>Seu c&#243;digo de recupera&#231;&#227;o de senha &#233;:</a>\r\n"
 				+ "                </div>\r\n"
-				+ "                <div style=\"margin-left: 41%; margin-top: 3%; margin-bottom: 3%; margin-right: 41%; padding: 1%; background-color: #EEE; text-align: center; border: 1px solid; border-color: black;\">\r\n"
-				+ "                    <a style=\"text-decoration: none;\">"+codigo+"</a>\r\n"
+				+ "                <div style=\"padding-left: 25%; margin-top: 3%; margin-bottom: 3%;\">\r\n"
+				+ "					<div style=\"text-align: center; background-color: #EEE; border: 1px solid; border-color: black; width: 25%; padding: 1%;\">\r\n"
+				+ "                    	<a style=\"text-decoration: none;\">"+codigo+"</a>\r\n"
+				+ "					</div>\r\n"
 				+ "                </div>\r\n"
 				+ "                <div>\r\n"
 				+ "		    		<a>Se o c&#243;digo n&#227;o funcionar,</a><br><br>\r\n"
@@ -55,7 +57,7 @@ public class sendEmail {
 			HtmlEmail javax = new HtmlEmail();
 			javax.setHostName("smtp-mail.outlook.com");
 			javax.setSmtpPort(587);
-			javax.setAuthentication("itonomise@hotmail.com", "A%3x!GYRqUt|Gy7N");
+			javax.setAuthentication("iTonomise@hotmail.com", "A%3x!GYRqUt|Gy7N");
 			javax.setStartTLSEnabled(true);
 			javax.setFrom("iTonomise@hotmail.com");
 			javax.addTo(email);
